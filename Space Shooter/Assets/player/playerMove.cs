cf4f4c9f0ebc872c286d2f439328606cf5fb30c1
pinejh <pinejh95@gmail.com>
2017-02-21 16:21:46 -0500

    added player shooting component and player shot

--- Space Shooter/Assets/player/playerMove.cs
+++ Space Shooter/Assets/player/playerMove.cs
@@ -8,6 +8,7 @@ public class playerMove : MonoBehaviour
 
     public GameObject player;
     public SpriteRenderer sprite;
+	public GameObject shot;
     //public Text uiLives;
     //public Text uiScore;
 
@@ -17,7 +18,7 @@ public class playerMove : MonoBehaviour
     public int startingLives;
     private int lives;
 
-    private bool left = false, right = false;
+	private bool left = false, right = false, space = false;
 
     public int invinceableTimer;
     private int invTimer;
@@ -25,6 +26,9 @@ public class playerMove : MonoBehaviour
 
     public float timer;
     private float score;
+
+	public int shootCooldown;
+	private int shootTimer;
 
 	// Use this for initialization
 	void Start ()
@@ -95,12 +99,31 @@ public class playerMove : MonoBehaviour
             }
         }
 
+		//tests if should shoot
+		if (space && shootTimer <= 0)
+		{
+			shoot();
+		}
+		else if (shootTimer > 0)
+		{
+			shootTimer--;
+		}
+
         //Update score variable
         score = timer / 10;
 
         //Update score text on screen
         //uiScore.text = "Score: " + Mathf.FloorToInt(score);
     }
+
+	/*Method instantiates new shot as GameObject in scene*/
+	public void shoot()
+	{
+		Instantiate(shot, new Vector3(player.transform.position.x, player.transform.position.y + .25f, player.transform.position.z), Quaternion.identity);
+
+		//shot cooldown
+		shootTimer = shootCooldown;
+	}
 
     /*Method run upon collision of an asteriod*/
     public void takeDamage()
@@ -124,6 +147,9 @@ public class playerMove : MonoBehaviour
             case KeyCode.D:
                 right = true;
                 break;
+			case KeyCode.Space:
+				space = true;
+				break;
         }
     }
 
@@ -137,7 +163,10 @@ public class playerMove : MonoBehaviour
                 break;
             case KeyCode.D:
                 right = false;
-                break;
+				break;
+			case KeyCode.Space:
+				space = false;
+				break;
         }
     }
 
---
+++ Space Shooter/Assets/player/playerShot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShot : MonoBehaviour {
+
+	public GameObject shot;
+	public PolygonCollider2D poly;
+
+	public float speed;
+	private float speedScale = 50;
+	
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		//tests if shot is off screen
+		Vector3 shotPos = new Vector3 (shot.transform.position.x, shot.transform.position.y - .5f, shot.transform.position.z);
+		Vector3 tempPos = Camera.main.WorldToScreenPoint(shotPos);
+		if (tempPos.y > Screen.height)
+		{
+			Destroy (shot);
+		}
+
+		//moves shot
+		shot.transform.Translate (0, speed / speedScale, 0);
+	}
+}
